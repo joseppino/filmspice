6da021f07696eea72edd79f1ef80a446da947bdd
Joseph Lloyd <joseph.lloyd17@hotmail.com>
2024-09-09 17:46:58 +0100

    some changes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import "$lib/global.css";
+  import "purecss";
+</script>
+
+
+<main>
+  <slot />
+</main>
+
+<style>
+  main {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { Toaster, toast } from "svelte-french-toast";
+
+  let fileInput: HTMLInputElement;
+
+  let files: FileList;
+  let file: File;
+  $: if(files) { file = files[0] };
+
+  function handleFileInputChange() {
+    console.log(files);
+    
+    if(files.length > 0) {
+      toast.success("File Upload Success!");
+    }
+  }
+
+</script>
+
+<Toaster /> 
+<h1>Filmspice</h1>
+<div>
+  <label for="photo">Upload a photo:</label>
+  <input accept="image/png, image/jpeg" id="photo" name="photo" type="file"
+    bind:files={files}
+    bind:this={fileInput}
+    on:change={handleFileInputChange}
+  />
+</div>
+
+{#if file}
+	<h2>Selected files:</h2>
+  <p>{file.name} ({file.size} bytes)</p>
+{/if}
+
+<button class="pure-button submit">Filmify!</button>
+
+<style>
+ .submit {
+    margin-top: 5px;
+ }
+</style>
